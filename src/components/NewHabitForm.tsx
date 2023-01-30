43a393dd4054c695483a24a9f6040b9341898ebe
Joel Luis <joelluis73@gmail.com>
2023-01-30 20:21:01 -0300

    submit new form

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react'
 import { Check } from 'phosphor-react'
 import * as Checkbox from '@radix-ui/react-checkbox'
 
@@ -12,8 +13,26 @@ const availableWeekDays = [
 ]
 
 export function NewHabitForm() {
+  const [newHabitTitle, setNewHabitTitle] = useState('')
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  function handleCreateNewHabit(event: FormEvent) {
+    event.preventDefault()
+    console.log(newHabitTitle)
+    setNewHabitTitle('')
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOnde = weekDays.filter((day) => day !== weekDay)
+      setWeekDays(weekDaysWithRemovedOnde)
+    } else {
+      setWeekDays([...weekDays, weekDay])
+    }
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={handleCreateNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
@@ -23,16 +42,19 @@ export function NewHabitForm() {
         placeholder="ex.: Exercícios, dormim bem, etc"
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        value={newHabitTitle}
+        onChange={(event) => setNewHabitTitle(event.target.value)}
       />
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map((weekDay) => (
+        {availableWeekDays.map((weekDay, index) => (
           <Checkbox.Root
             key={weekDay}
             className="flex items-center gap-3 group"
+            onCheckedChange={() => handleToggleWeekDay(index)}
           >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
               <Checkbox.Indicator>
